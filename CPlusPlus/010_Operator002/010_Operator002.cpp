260de4ae6300166dd20b129d6643e65016687c81
GuSungMo <coincheen@gmail.com>
2023-03-13 22:57:06 +0900

    구성모 : 3월 13일 수업후

--- CPlusPlus/010_Operator002/010_Operator002.cpp
+++ CPlusPlus/010_Operator002/010_Operator002.cpp
@@ -1,20 +1,67 @@
-﻿// 010_Operator002.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int Left = 0b00000000000000000000000000000010;
+	int Right = 0b00000000000000000000000000000001;
+	int Result = 0b00000000000000000000000000000000;
+
+	// 비트단위 연산자.
+	// 0b00000000000000000000000000000000
+	// Not
+	Result = ~0;
+
+	// 0b00000000000000000000000000000010
+	// 0b00000000000000000000000000000001
+	// 0b00000000000000000000000000000000
+	// and
+	Result = Left & Right;
+	// or
+	Result = Left | Right;
+	// xor // 둘이 다를때만 true
+	Result = Left | Right;
+	// 둘이 1로 같든 0으로 같은 같으면 false가 된다.
+	Result = 3 ^ 3;
+
+	// true와 false 참과 거짓의 명확한 의미를 
+	// 한번 짚고 넘어가겠습니다.
+	// 특정 메모리 영역의 비트가 전부 0이라면 false 와 거짓
+	// 특정 메모리 영역의 비트중 단 1개라도 1이 있다면 true 와 참
+	bool bResult = 2;
+	bResult = 0 && 10021312;
+
+	// 0b00000000000000000000000000001000
+	Result = 1 << 3;
+
+	// 이런식의 어떤일이 발생할지 알수 없는 코드는 애초에 쓰지 않아요.
+	Result = 1 << 33;
+	Result = 1 >> 1;
+
+	// 복합할당 연산자.
+	// 특정 메모리영역의 값을 기반으로 하는 이항 연산자
+
+	//덧셈 대입	a += b	예	예	T & T::operator+=(const T & b);	T& operator+=(T & a, const T & b);
+	//뺄셈 대입	a -= b	예	예	T & T::operator-=(const T & b);	T& operator-=(T & a, const T & b);
+	//곱셈 대입	a *= b	예	예	T & T::operator*=(const T & b);	T& operator*=(T & a, const T & b);
+	//나눗셈 대입	a /= b	예	예	T & T::operator/=(const T & b);	T& operator/=(T & a, const T & b);
+	//모듈러 대입	a %= b	예	예	T & T::operator%=(const T & b);	T& operator%=(T & a, const T & b);
+	//비트 AND 대입	a &= b	예	예	T & T::operator&=(const T & b);	T& operator&=(T & a, const T & b);
+	//비트 OR 대입	a |= b	예	예	T & T::operator|=(const T & b);	T& operator|=(T & a, const T & b);
+	//비트 XOR 대입	a ^= b	예	예	T & T::operator^=(const T & b);	T& operator^=(T & a, const T & b);
+	//비트 왼쪽 시프트 대입	a <<= b	예	예	T & T::operator<<=(const T & b);	T& operator<<=(T & a, const T & b);
+	//비트 오른쪽 시프트 대입	a >>= b	예	예	T & T::operator>>=(const T & b);	T& operator>>=(T & a, const T & b);
+
+	int Value = 20;
+
+	Value = Value + 10;
+
+	// 복합
+	Value += Value;
+	Value = 1;
+
+	Value = Value << 3;
+
+	// heal
+	Value = Value + 30;
+	Value += 30;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
